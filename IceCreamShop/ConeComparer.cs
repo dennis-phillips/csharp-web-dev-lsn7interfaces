4fd83dbd4b7bc986ccd7abad625cba782c1cd3e0
dennis-phillips <dennis_phillips@outlook.com>
2020-06-11 16:41:09 -0400

    Unit 8 Exercises complete

---
+++ IceCreamShop/ConeComparer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IceCreamShop
+{
+    public class ConeComparer : IComparer<Cone>
+    {
+        public int Compare(Cone x, Cone y)
+        {
+            double diff = x.Cost - y.Cost;
+            if (diff == 0)
+            {
+                return 0;
+            }
+            if (diff < 0)
+            {
+                return -1;
+            }
+            return 1;
+            
+            
+            
+        }
+    }
+}
--- IceCreamShop/Program.cs
+++ IceCreamShop/Program.cs
@@ -11,13 +11,27 @@ namespace IceCreamShop
             List<Flavor> availableFlavors = menu.Flavors;
             List<Cone> availableCones = menu.Cones;
 
-        // TODO: Use a Comparer class to sort the 'flavors' array alphabetically by the 'name'
-        //  field.
+            // TODO: Use a Comparer class to sort the 'flavors' array alphabetically by the 'name'
+            //  field.
+            FlavorComparer comparer = new FlavorComparer();
+            availableFlavors.Sort(comparer);
 
-        // TODO: Use a Comparator class to sort the 'cones' array in increasing order by the 'cost'
-        //  field.
+            // TODO: Use a Comparator class to sort the 'cones' array in increasing order by the 'cost'
+            //  field.
+            ConeComparer coneComparer = new ConeComparer();
+            availableCones.Sort(coneComparer);
 
-        // TODO: Print the 'flavors' and 'cones' lists (in a clear manner) to verify the sorting.
+            availableCones.Sort((x, y) => x.Cost.CompareTo(y.Cost));
+
+            // TODO: Print the 'flavors' and 'cones' lists (in a clear manner) to verify the sorting.
+            foreach (var flavor in availableFlavors)
+            {
+                Console.WriteLine(flavor.Name);
+            }
+            foreach (var cone in availableCones)
+            {
+                Console.WriteLine($"{cone.Name}: {cone.Cost} ");
+            }
         }
     }
 }
